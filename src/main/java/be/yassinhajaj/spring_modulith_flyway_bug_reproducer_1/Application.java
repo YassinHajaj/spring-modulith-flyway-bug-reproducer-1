d329f32a813112eff4ab4db6d285d13ca8b1a480
Yassin Hajaj <yassin@thegreenerfield.be>
2026-03-17 12:17:12 +0100

    initialize Spring Boot application with Maven wrapper and basic project structure

---
+++ src/main/java/be/yassinhajaj/spring_modulith_flyway_bug_reproducer_1/Application.java
@@ -0,0 +1,13 @@
+package be.yassinhajaj.spring_modulith_flyway_bug_reproducer_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/be/yassinhajaj/spring_modulith_flyway_bug_reproducer_1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.yassinhajaj.spring_modulith_flyway_bug_reproducer_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
